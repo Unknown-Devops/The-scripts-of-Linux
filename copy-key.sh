7eff10700be069877a6b5c938379747bff832885
Unabled <yumengyang426@163.com>
2018-08-22 20:55:38 +0800

    ssh-copy-id and certification  by ansible and expert
    分发秘钥后，为了解决ansible第一次连接还是要确认主机的问题，用expert做一个自动应答，认证一次，建立known_hosst文件

--- copy-key.sh
+++ copy-key.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
 # copy the ssh-key to all hosts
 #$1=host_name $2=password_server
-#v3
+#use ansible and expect to achieve certification of hosts
+#v4
 
 for host in `grep "$1" /etc/hosts |awk '{print $1}'`
 do
 	/usr/bin/sshpass -p "$2" ssh-copy-id root@${host}
 
 done
+
+/usr/bin/expect <<-EOF
+	spawn	/usr/bin/ansible $1 -m ping
+	expect {
+		"yes/no" { send "yes\r"; exp_continue }
+	}
+	expect eof
+EOF
